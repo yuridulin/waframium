331c9583ec730820ef4039fffbce7334514e4222
Юрий Дулин <u.dulin@vitebsk.energo.net>
2024-07-17 12:09:08 +0300

    Пробуем запихнуть реальную функцию

--- R/loader.R
+++ R/loader.R
@@ -6,13 +6,21 @@ say_hello <- function() {
   print("HELLO FROM waframium")
 }
 
-#' Функция для загрузки данных
-#'
-#' Функция для загрузки данных
-#' @export
-load_project <- function () {
-  projects <- reticulate::import_from_path("./python/projects.py")
-  loader <- reticulate::import_from_path("./python/loader.py")
 
-  dynamic <-loader$loadDynamic(P_22EF02, include_dead=False)
+#' Функция показада начала и конца диапазона с пациентами, хуй знает зачем, я учюсь кароч
+#'
+#' Функция показада начала и конца диапазона с пациентами, хуй знает зачем, я учюсь кароч
+#' @export
+visit_subjects_dates <- function(df, subjects = "system_patient_id", dates = "visit_time",  format_ = '%Y.%m.%d') {
+
+  df <- df %>%
+    select(subjects = !!sym(subjects), dates = !!sym(dates)) %>%
+    arrange(dates)
+
+  min_d <- format(df[1, 2] , format = format_)
+  min_s <- df[1,1]
+  max_d <- format(df[nrow(df), 2] , format = format_)
+  max_s <- df[nrow(df), 1]
+
+  return(paste0("c ", min_d, " [#", min_s, "] по ", max_d, " [#", max_s, "]"))
 }
